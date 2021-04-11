93816bfb199ad321426feeff3bcd12e00c8d71c8
Michael Janks <mjanks@Michaels-MacBook-Pro.local>
2021-04-11 00:31:23 -0400

    propertyDetails screen now displays all reviews for that particular property

--- screens/propertyDetails.js
+++ screens/propertyDetails.js
@@ -1,11 +1,12 @@
-import React from "react";
-import { View, Text, Button, Image } from "react-native";
+import React, { useState } from "react";
+import { FlatList, View, Text, Button, Image } from "react-native";
 import Card from '../shared/card';
 import { globalStyles, images } from '../styles/global';
 
 const PropertyDetails = ({ route, navigation }) => {
   const { PropertyId, Price, StreetAddress, City, State, Zipcode, Title, Description, NumBeds, NumBaths, Rating } = route.params;
   const property = { PropertyId, Price, StreetAddress, City, State, Zipcode, Title, Description, NumBeds, NumBaths, Rating };
+  const [data, setData] = useState(null);
 
   const pressHandler = () => {
 
@@ -22,6 +23,34 @@ const PropertyDetails = ({ route, navigation }) => {
     navigation.navigate('Home');
   }
 
+  const getData = () => {
+    fetch('https://michaeljanks.com/getReviews.php', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json" ,
+        'Cache-control': 'no-cache, no-store, must-revalidate', 
+        'pragma': 'no-cache'
+      },
+      body: JSON.stringify(property)
+    })
+    .then(res => {
+      if(!res.ok) {
+        throw Error('ERROR: Could not fetch data!');
+      }
+      return res.json();
+    })
+    .then(data => {
+      setData(data);
+      console.log(JSON.stringify(property));
+    })
+    .catch(err => {
+      if (err.name === 'AbortError') {
+        console.log('fetch aborted');
+      }
+    })
+  }
+
+
   return (
     <View style={globalStyles.propDetails}>
 
@@ -37,10 +66,6 @@ const PropertyDetails = ({ route, navigation }) => {
       <Text>Description: { Description }</Text>
       <Text>Number of beds: { NumBeds }</Text>
       <Text>Number of baths: { NumBaths }</Text>
-      <View style={globalStyles.rating}>
-          <Text>Rating:</Text>
-          <Image source={images.ratings[Rating]} />
-      </View>
       
       </Card>
       <View style={globalStyles.buttonContainer}>
@@ -50,6 +75,23 @@ const PropertyDetails = ({ route, navigation }) => {
       <View style={globalStyles.buttonContainer}>
         <Button title='Write a review' color='#cc1111' onPress={() => navigation.navigate('WriteReview', property)} />
       </View>
+
+      <Button title='Tap to see reviews of this property' onPress={getData}/>
+      <FlatList
+          data={data}
+          keyExtractor={(item, index) => item.ReviewId}
+          renderItem={({ item }) => (
+              <Card>
+              <Text>Title: { item.Title }</Text>
+              <Text>Review: { item.Text }</Text>
+              <Text>Time Posted: { item.TimePosted }</Text>
+              <View style={globalStyles.rating}>
+                <Text>Rating:</Text>
+                <Image source={images.ratings[Rating]} />
+              </View>
+              </Card>
+          )}
+      /> 
 
     </View>
   );
